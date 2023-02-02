32d3f96dc1246396f9fb960bab9f9aaf3d18e66b
Matt Hansen <2936866+m-hansen@users.noreply.github.com>
2023-02-02 07:35:15 -0500

    Remove settlement highlighting on hover

--- Assets/Scripts/Settlement.cs
+++ Assets/Scripts/Settlement.cs
@@ -5,39 +5,11 @@ public class Settlement : MonoBehaviour
 {
     private const float CostMultiplier = 3.5f; // temporary to pad numbers
 
-    [SerializeField]
-    private Color hoverColor;
-
-    private SpriteRenderer spriteRenderer;
-    private Color defaultColor = Color.magenta; // magenta is used as an error color - we may fail to find the correct default color
-
-    private bool isHoveredOver = false; // old code used to highlight nodes, will likely be removed soon in favor of a different technique
-
     public int Cost { get; private set; }
-
-    private void Awake()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        Debug.Assert(spriteRenderer != null);
-
-        defaultColor = spriteRenderer.color;
-    }
 
     private void Start()
     {
         Cost = (int)CalculateBaseCost(); // truncate our generated cost for simplicity
-    }
-
-    private void OnMouseEnter()
-    {
-        isHoveredOver = true;
-        spriteRenderer.color = hoverColor;
-    }
-
-    private void OnMouseExit()
-    {
-        spriteRenderer.color = defaultColor;
-        isHoveredOver = false;
     }
 
     private float CalculateBaseCost()
