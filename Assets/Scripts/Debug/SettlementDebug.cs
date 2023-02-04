3aed3ead4b6d89b7e8fb48557a2023b34b89b870
Matt Hansen <2936866+m-hansen@users.noreply.github.com>
2023-02-04 08:55:56 -0500

    Add settlement id to the debug UI

--- Assets/Scripts/Debug/SettlementDebug.cs
+++ Assets/Scripts/Debug/SettlementDebug.cs
@@ -5,22 +5,23 @@ public class SettlementDebug : MonoBehaviour
 {
     public TMPro.TMP_Text cost;
     public TMPro.TMP_Text resource;
+    public TMPro.TMP_Text id;
     public Image resourcImg;
     public Settlement settlementScript;
 
     void Update()
     {
+        id.text = settlementScript.Id.ToString();
         cost.text = settlementScript.Cost.ToString();
-        if(settlementScript.hasResource)
+
+        if (settlementScript.hasResource)
         {
             resourcImg.enabled= true;
             if (settlementScript.IsConnectedToPowerPlant())
             {
                 resource.enabled = true;
                 resource.text = $"+{settlementScript.GetResourcePerSecond()}/s";
-                
             }
         }
-
     }
 }
