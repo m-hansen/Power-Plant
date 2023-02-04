87dce3f33e7a31ebd93dd07f2f80b40e5b672633
Dogincan <timurg.lmc@gmail.com>
2023-02-04 06:29:43 -0800

    +added wip button imgs

--- Assets/Scripts/Debug/SettlementDebug.cs
+++ Assets/Scripts/Debug/SettlementDebug.cs
@@ -5,23 +5,36 @@ public class SettlementDebug : MonoBehaviour
 {
     public TMPro.TMP_Text cost;
     public TMPro.TMP_Text resource;
-    public TMPro.TMP_Text id;
     public Image resourcImg;
+    public Image plusImg;
+    public Image minusImg;
+    public TMPro.TMP_Text id;
     public Settlement settlementScript;
+
 
     void Update()
     {
         id.text = "ID: " + settlementScript.Id.ToString();
         cost.text = settlementScript.Cost.ToString();
-
         if (settlementScript.hasResource)
         {
-            resourcImg.enabled= true;
+            resourcImg.enabled = true;
             if (settlementScript.IsConnectedToPowerPlant())
             {
                 resource.enabled = true;
                 resource.text = $"+{settlementScript.GetResourcePerSecond()}/s";
+
             }
+        }
+        if (settlementScript.IsConnectedToPowerPlant())
+        {
+            plusImg.enabled = true;
+            minusImg.enabled = true;
+            cost.enabled = false;
+        }
+        else
+        {
+            cost.text = settlementScript.Cost.ToString();
         }
     }
 }
