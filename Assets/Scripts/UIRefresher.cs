172fdb15ff36bd699d707836b876b952d6087256
Matt Hansen <2936866+m-hansen@users.noreply.github.com>
2023-02-05 14:07:34 -0500

    Add toggle how to window at start of game with exit button

--- Assets/Scripts/UIRefresher.cs
+++ Assets/Scripts/UIRefresher.cs
@@ -6,13 +6,30 @@ public class UIRefresher : MonoBehaviour
     [SerializeField]
     private TMP_Text gameplayTimeLabel;
 
+    [SerializeField]
+    private GameObject howToPlayPanel;
+
     private void Start()
     {
         InvokeRepeating("UpdateGameTimeUI", 0.001f, 1f); // the user doesn't need to see ultra accurate time updates, but it will still be stored on the backend
     }
 
+    private void Update()
+    {
+        // Toggle how to play 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            howToPlayPanel.SetActive(!howToPlayPanel.activeInHierarchy);
+        }
+    }
+
     private void UpdateGameTimeUI()
     {
         gameplayTimeLabel.text = GameManager.Instance.GameplayTimer.FormattedTime;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
